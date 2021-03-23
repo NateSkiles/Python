9d63164fcd2c04b96b28ed78a3add1ee4d3afb2a
Nathan Skiles <nateskiles94@gmail.com>
2021-03-23 14:56:03 -0700

    3/23

---
+++ databaseAndPython/databaseAssignment.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('dbAssignment.db')
+
+fileList = ('information.dox', 'hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhono.jpg')
+y = '.txt'
+
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS tbl_files( \
+                    FileID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                    col_fileName TEXT)')
+    conn.commit()
+conn.close
+
+
+
+for file in fileList:
+    if file.endswith(y):
+        with conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO tbl_files (col_fileName) VALUES (?)", (file,))
+                conn.commit()
+        print(file)
+        conn.close
+        
+
+        
+    
