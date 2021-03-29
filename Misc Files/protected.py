3a7ccd859d3d8d61ac75258e20efbbec2a406a60
Nathan Skiles <nateskiles94@gmail.com>
2021-03-29 13:03:06 -0700

    Update protected.py

--- Misc Files/protected.py
+++ Misc Files/protected.py
@@ -2,9 +2,9 @@ class Protected:
     def __init__(self):
         self._protected = 0  # This is my protected attribute. It can be accessed and modified
         self.__privateVar = 0  # This is my private attribute. It can still be modified or read, but..
-        # we include methods to do so, this prevents the variable from changing on accident.
+        # we include methods to do so, this prevents the variable from changing on accident
 
-    def getPrivate(self):
+    def getPrivate(self):   # Functions to access and modify my private var
         print(self.__privateVar)
 
     def setPrivate(self, setValue):
