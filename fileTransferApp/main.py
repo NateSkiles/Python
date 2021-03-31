fea6996a39dd02d67945d43fa824436d34027cf5
Nathan Skiles <nateskiles94@gmail.com>
2021-03-31 13:54:12 -0700

    Update main.py

--- fileTransferApp/main.py
+++ fileTransferApp/main.py
@@ -1,3 +1,13 @@
+# Python Ver:   3.9.2
+#
+# Author:       Nathan Skiles
+#
+# Purpose:      Your employer wants a program to move all their text files (with a .txt file extension) from one
+#               folder to another with the click of a button. On your desktop you will have two new folders, one
+#               called Folder A and another called Folder B. You will need to move text files from Folder A to Folder B.
+#
+# Tested OS:    This code was written & tested to work with Windows 10
+
 from tkinter import *
 import tkinter as tk
 import fileTransfer_GUI
